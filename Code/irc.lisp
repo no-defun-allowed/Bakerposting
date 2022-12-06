623f0aa2cedb0a9716dfa1ab998d6ec6601f0279
Hayley Patton <theemacsshibe@gmail.com>
2022-12-06 18:27:20 +1100

    Use Maiden for IRC, which has been more stable.

--- Code/irc.lisp
+++ Code/irc.lisp
@@ -1,38 +1,35 @@
 (in-package :bakerposting)
 
-(defvar *irc*)
+(maiden:define-consumer baking () ())
 
-(defun irc-reply (message)
-  (destructuring-bind (channel text)
-      (irc:arguments message)
+(defun reply-with-notice (e message)
+  (maiden-irc-events:notice
+   (maiden-client-entities:client e)
+   (maiden:name
+    (maiden-client-entities:channel e))
+   message))
+
+(maiden:define-handler (baking maybe-reply maiden-client-entities:message-event) (c e)
+  (let ((text (maiden-client-entities:message e)))
     (when (or (search "pony" text) (search "ponies" text))
-      (irc:notice *irc* channel "OMG!! Ponies!!!"))
+      (reply-with-notice e "OMG!! Ponies!!!"))
     (when (and (search "Bakerposting" text)
                (not (search "'Bakerposting" text))
                (not (search "(quote Bakerposting)" text)))
-      (irc:notice *irc*
-                  channel
-                  (if (search "uptime" text)
-                      (uptime)
-                      (remove-newlines (random-quote :length-limit 300))))))
-  t)
+      (reply-with-notice
+       e
+       (if (search "uptime" text)
+           (uptime)
+           (remove-newlines (random-quote :length-limit 300)))))))
 
-(defun reconnect ()
-  (declare (ignore message))
-  (setf *irc*
-        (irc:connect
-         :server "irc.libera.chat"
-         :nickname "Bakerposting"
-         :password (getf *config* :irc-password)))
-  (dolist (c (getf *config* :irc-channels))
-    (irc:join *irc* c))
-  (irc:add-hook *irc* 'irc:irc-privmsg-message 'irc-reply)
-  (irc:add-hook *irc* 'irc:irc-quit-message
-                (lambda (m)
-                  (declare (ignore m))
-                  (sleep 3)
-                  (reconnect)))
-  (bt:make-thread (lambda () (irc:read-message-loop *irc*))
-                  :name "IRC listener"))
+(defvar *core*)
 
-(define-hook irc (reconnect))
+(define-hook irc
+  (setf *core*
+        (maiden:make-core
+         `(:maiden-irc
+           :nickname "Bakerposting"
+           :password ,(getf *config* :irc-password)
+           :host "irc.libera.chat"
+           :channels ,(getf *config* :irc-channels))
+         'baking)))
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 This posts Baker.
 
 The bot itself is licensed under BSD 2-clause, but the articles in
-`Assets/Articles/` are of course the property of Henry G. Baker. Under
+`Assets/Articles/` are of course the property of Henry G. Baker, and
+the author's transcription of Erlang: the Movie is probably the property
+of Ericsson if she had to guess. Under
 [Australian law](https://www.alrc.gov.au/publication/copyright-and-the-digital-economy-ip-42/fair-dealing-exceptions/)
 the corpus is considered by the author to be *fair dealing* as it used
 for study.
@@ -10,5 +12,3 @@ for study.
 
 - Matrix backend.
 - Either a Mastodon or Pleroma backend.
-- Investigate why CL-IRC likes to eat CPU time after a QUIT message.
-  Actually, I think it's an SBCL thing. Oh dear.
